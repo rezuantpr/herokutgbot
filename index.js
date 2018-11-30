612c4f7ffda158dadc6a276651b050522aacc989
rezuantpr <rbbox@toprands.xyz>
2018-11-30 15:17:18 +0300

    v2

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const TOKEN = '777947545:AAGZjIaHUpgcl5Za0YUt4aBbHL9uG7yCkag'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, `Hello from HEROKU, bot says: "Hi, ${msg.from.first_name}"`)
-  
+  bot.sendMessage(msg.chat.id, `${msg.from.first_name}, иди нахуй!`)
+
 })
