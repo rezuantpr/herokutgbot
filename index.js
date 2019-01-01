741c6381d5c85e2bad2986d4d1d3c82720346cd4
rezuantpr <rbbox@toprands.xyz>
2019-01-01 20:38:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const TOKEN = '777947545:AAGZjIaHUpgcl5Za0YUt4aBbHL9uG7yCkag'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, `${msg.from.first_name}, иди нахуй!!!`)
+  bot.sendMessage(msg.chat.id, `${msg.from.first_name}, hello!!!`)
 
 })
